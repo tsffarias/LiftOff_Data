d8ff994ba63a071af7ec5d60cf7ec1e8c4648bcb
Thiago Silva Farias <thiago_protect@hotmail.com>
2024-10-12 22:26:29 -0300

    Fix: image frontend

--- app/frontend/app.py
+++ app/frontend/app.py
@@ -632,7 +632,7 @@ class Dashboard:
         7. Plataforma 'Briefer' para acesso e utilização dos dados transformados
         """)
 
-        st.image("https://raw.githubusercontent.com/tsffarias/LiftOff_Data/refs/heads/main/img/arquitetura.png", use_column_width=True, caption="Arquitetura do Pipeline de Dados")
+        st.image("https://raw.githubusercontent.com/tsffarias/LiftOff_Data/refs/heads/main/img/arquitetura_1.1.png", use_column_width=True, caption="Arquitetura do Pipeline de Dados")
 
         st.divider()
 
