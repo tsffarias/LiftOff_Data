15b3a92c6fd3c95439d44ae54f4102e88d5e0e0f
Thiago Silva Farias <thiago_protect@hotmail.com>
2024-11-15 13:14:57 -0300

    Feat: Selecting email fornecedor on product crud page

--- app/frontend/product/create.py
+++ app/frontend/product/create.py
@@ -11,6 +11,18 @@ from utils import show_response_message
 load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
 
 def create():
+    # Buscar a lista de fornecedores
+    response_suppliers = requests.get(f"{os.getenv('BACKEND_URL')}/suppliers/")
+    if response_suppliers.status_code == 200:
+        suppliers = response_suppliers.json()
+        # Extrair os emails dos fornecedores
+        supplier_emails = [supplier['email'] for supplier in suppliers]
+        # Inserir "Selecione o Email do Fornecedor" no início da lista
+        supplier_emails.insert(0, "Selecione o Email do Fornecedor")
+    else:
+        show_response_message(response_suppliers)
+        supplier_emails = ["Selecione o Email do Fornecedor"]
+
     with st.form("new_product"):
         name = st.text_input("Nome do Produto")
         description = st.text_area("Descrição do Produto")
@@ -19,18 +31,22 @@ def create():
             "Categoria",
             ["Eletrônico", "Eletrodoméstico", "Móveis", "Roupas", "Calçados"],
         )
-        email_fornecedor = st.text_input("Email do Fornecedor")
+        email_fornecedor = st.selectbox("Email do Fornecedor", options=supplier_emails)
         submit_button = st.form_submit_button("Adicionar Produto")
 
         if submit_button:
-            response = requests.post(
-                f"{os.getenv('BACKEND_URL')}/products/",
-                json={
-                    "name": name,
-                    "description": description,
-                    "price": price,
-                    "categoria": categoria,
-                    "email_fornecedor": email_fornecedor,
-                },
-            )
-            show_response_message(response)
+            # Verificação antes de enviar o formulário
+            if email_fornecedor == "Selecione o Email do Fornecedor":
+                st.warning("Por favor, selecione um email válido do fornecedor.")
+            else:
+                response = requests.post(
+                    f"{os.getenv('BACKEND_URL')}/products/",
+                    json={
+                        "name": name,
+                        "description": description,
+                        "price": price,
+                        "categoria": categoria,
+                        "email_fornecedor": email_fornecedor,
+                    },
+                )
+                show_response_message(response)
